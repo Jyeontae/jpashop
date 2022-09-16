4279dc381c1e8b5e54e47ae34f969477410b8bd8
Yeontae Jeong <jyeontae>
2022-09-16 20:28:07 +0900

    API개발 고급{NToMany DTO해결법(페치 조인, batch_size)}

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -116,4 +116,13 @@ public class OrderRepository {
                                 " join o.delivery d", OrderQuerySimpleDto.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery("select  o from Order o "+
+                                        "join fetch o.member m "+
+                                        "join fetch o.delivery d "+
+                                        "join fetch o.orderItems oi "+
+                                        "join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
