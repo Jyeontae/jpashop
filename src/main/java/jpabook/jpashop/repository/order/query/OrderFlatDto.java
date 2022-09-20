87d72a6efeccafa56b81dbdb35688a02c175e141
Yeontae Jeong <jyeontae>
2022-09-20 17:47:22 +0900

    API개발 고급{NToMany DTO해결법}

--- src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -1,16 +1,14 @@
-package jpabook.jpashop.repository;
+package jpabook.jpashop.repository.order.query;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
-public class OrderQuerySimpleDto {
+public class OrderFlatDto {
 
     private Long orderId;
     private String name;
@@ -18,11 +16,18 @@ public class OrderQuerySimpleDto {
     private OrderStatus orderStatus;
     private Address address;
 
-    public OrderQuerySimpleDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address){
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, String itemName, int orderPrice, int count) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
     }
 }
